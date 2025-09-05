2d671d41613bc9cc4189df8984f3c8ae4b33fec5
davi gerson <dgas1@discente.ifpe.edu.br>
2025-09-04 21:24:28 -0300

    editando o app filme

--- filme/models.py
+++ filme/models.py
@@ -2,9 +2,23 @@ from django.db import models
 
 # Create your models here.
 
-#criar o filme
 
-#criar os episodios
+# criar o filme
+class Filme(models.Model):
+  tituto = models.CharField(max_length=256) #Campo de texto;
+  thumb = 
+  descricao = models.TextField(max_length=1000) #Bloco de texto
+  categoria = models.
+  visualizacoes = 
+  data_criacao = 
+   
+  -Thumb
+  -Descrição
+  -Categoria
+  -quantidade de views
+  -data de publicação 
 
-#criar o usuario
+# criar os episodios
+
+# criar o usuario
 
