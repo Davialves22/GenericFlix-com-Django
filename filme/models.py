94b0f9fcb1f67ad92fc1262ff272f28ffa8f79f7
davi gerson <dgas1@discente.ifpe.edu.br>
2025-09-10 21:54:55 -0300

    configurando pastas static e media

--- filme/models.py
+++ filme/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils import timezone
 
-
 # Create your models here.
 
 LISTA_CATEGORIAS = (
@@ -15,13 +14,17 @@ LISTA_CATEGORIAS = (
 
 # criar o filme
 class Filme(models.Model):
-    tituto = models.CharField(max_length=256)  # Campo de texto;
-    thumb = models.ImageField(upload_to='thumb_filmes') #campo de imagem;
+    titulo = models.CharField(max_length=256)  # Campo de texto;
+    thumb = models.ImageField(upload_to='thumb_filmes')  # campo de imagem;
     descricao = models.TextField(max_length=1000)  # Bloco de texto
     categoria = models.CharField(max_length=15, choices=LISTA_CATEGORIAS)
     visualizacoes = models.IntegerField(default=0) 
     data_criacao = models.DateTimeField(default=timezone.now)
 
+# função padrão para formato de string
+    def __str__(self):  # vai retornar uma instancia da classe filme do proprio filme
+      return self.titulo
+  
 # criar os episodios
 
 # criar o usuario
