e258ec6d65bdd0fbd849b8873fb3c719412a86c9
davi gerson <dgas1@discente.ifpe.edu.br>
2025-09-10 21:02:46 -0300

    criando modelo de filme  e adicionando tabelas no banco de dados

--- filme/models.py
+++ filme/models.py
@@ -1,24 +1,27 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+
+LISTA_CATEGORIAS = (
+    # (armazenar_no_bd, aparecer_pra_o_usuario),
+    ("FILMES", "Filmes"),
+    ("SERIES", "Séries"),
+    ("ANIMES", "Animes"),
+    ("OUTROS", "Outros"),
+)
 
 
 # criar o filme
 class Filme(models.Model):
-  tituto = models.CharField(max_length=256) #Campo de texto;
-  thumb = 
-  descricao = models.TextField(max_length=1000) #Bloco de texto
-  categoria = models.
-  visualizacoes = 
-  data_criacao = 
-   
-  -Thumb
-  -Descrição
-  -Categoria
-  -quantidade de views
-  -data de publicação 
+    tituto = models.CharField(max_length=256)  # Campo de texto;
+    thumb = models.ImageField(upload_to='thumb_filmes') #campo de imagem;
+    descricao = models.TextField(max_length=1000)  # Bloco de texto
+    categoria = models.CharField(max_length=15, choices=LISTA_CATEGORIAS)
+    visualizacoes = models.IntegerField(default=0) 
+    data_criacao = models.DateTimeField(default=timezone.now)
 
 # criar os episodios
 
 # criar o usuario
-
