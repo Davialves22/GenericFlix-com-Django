df5c99ff308ab127a475faf61a347a8b751e19eb
davi gerson <dgas1@discente.ifpe.edu.br>
2025-10-01 21:10:39 -0300

    criado tabela de series,episodios e chave estrangeira

--- filme/admin.py
+++ filme/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Filme
+from .models import Filme, Serie, Episodio
 
 # Register your models here.
 admin.site.register(Filme)
+admin.site.register(Serie)
+admin.site.register(Episodio)
--- filme/models.py
+++ filme/models.py
@@ -8,16 +8,45 @@ LISTA_CATEGORIAS = (
     ("TERROR", "Terror"),
     ("SUSPENSE", "Suspense"),
     ("COMEDIA", "Comédia"),
+    ("ROMANCE", "Romance"),
+    ("ACAO", "Ação"),
+    ("AVENTURA", "Aventura"),
+    ("FICCAO_CIENTIFICA", "Ficção Científica"),
+    ("FANTASIA", "Fantasia"),
+    ("DRAMA", "Drama"),
+    ("MISTERIO", "Mistério"),
+    ("ANIMACAO", "Animação"),
+    ("ANIME", "Anime"),
+    ("DOCUMENTARIO", "Documentário"),
+    ("BIOGRAFIA", "Biografia"),
+    ("MUSICAL", "Musical"),
+    ("GUERRA", "Guerra"),
+    ("CRIME", "Crime"),
+    ("FAMILIA", "Família"),
+    ("HISTORIA", "História"),
+    ("FAROESTE", "Faroeste"),
     ("OUTROS", "Outros"),
 )
 
+
+#criar as series
+class Serie(models.Model):
+    titulo = models.CharField(max_length=100)
+    thumb = models.ImageField(upload_to="thumb_series")
+    descricao = models.TextField(max_length=500)
+    categoria = models.CharField(max_length=100, choices=LISTA_CATEGORIAS)
+    visualizacoes = models.IntegerField(default=0)
+    data_criacao = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.titulo
 
 # criar o filme
 class Filme(models.Model):
     titulo = models.CharField(max_length=256)  # Campo de texto;
     thumb = models.ImageField(upload_to='thumb_filmes')  # campo de imagem;
     descricao = models.TextField(max_length=1000)  # Bloco de texto
-    categoria = models.CharField(max_length=15, choices=LISTA_CATEGORIAS)
+    categoria = models.CharField(max_length=100, choices=LISTA_CATEGORIAS)
     visualizacoes = models.IntegerField(default=0) 
     data_criacao = models.DateTimeField(default=timezone.now)
 
@@ -26,5 +55,12 @@ class Filme(models.Model):
       return self.titulo
   
 # criar os episodios
+class Episodio(models.Model):
+    serie = models.ForeignKey("Serie", related_name="episodios", on_delete=models.CASCADE) #chave estrangeira de filme
+    titulo = models.CharField(max_length=256)
+    video = models.URLField()
+
+    def __str__(self):
+        return self.serie.titulo + " - " + self.titulo
 
 # criar o usuario
