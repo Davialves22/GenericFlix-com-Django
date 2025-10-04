436bda99075e61d8204505e53bfef745a4e14527
davi gerson <dgas1@discente.ifpe.edu.br>
2025-10-04 11:41:20 -0300

    adicionado filme recente como destaque e definido ajuste na navbar

--- filme/novos_context.py
+++ filme/novos_context.py
@@ -1,8 +1,10 @@
 from .models import Filme
 
+#função para filmes recentes e filme em destaque
 def lista_filmes_recentes(request):
     lista_filmes = Filme.objects.all().order_by('-data_criacao')[0:10]
-    return {"lista_filmes_recentes": lista_filmes}
+    filme_destaque = lista_filmes[0]
+    return {"lista_filmes_recentes": lista_filmes, "filme_destaque": filme_destaque}
 
 #função para filtrar por objetos em alta
 def lista_filmes_emalta(request):
