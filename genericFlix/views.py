717d97101b384c96ba3f180e109994a241875662
davi gerson <dgas1@discente.ifpe.edu.br>
2025-10-04 14:01:04 -0300

    agora sei como formatar no pycharm

--- genericFlix/views.py
+++ genericFlix/views.py
@@ -2,9 +2,6 @@
 
 from django.views.generic import ListView
 from django.db.models import Q
-
-# IMPORTANTE: Importe os modelos de seus respectivos apps
-# Certifique-se de que os nomes dos apps ('serie' e 'filme') estão corretos.
 from serie.models import Serie
 from filme.models import Filme
 
@@ -12,29 +9,36 @@ from filme.models import Filme
 class PesquisaUnificada(ListView):
     template_name = "pesquisa.html"
     model = None
-    context_object_name = 'resultados'  # Nome da variável no template
+    context_object_name = 'resultados'
 
     def get_queryset(self):
-        # A view espera o parâmetro 'q'
         query = self.request.GET.get('q')
-
         resultados_finais = []
 
-        if query:
-            # Filtro para buscar no campo 'titulo' (case-insensitive contains)
+        # 1. TRATAMENTO PARA BUSCA VAZIA (NOVO CÓDIGO AQUI)
+        if not query:
+            # Se a busca estiver vazia (ou for a primeira visita sem termo)
+            # Retorna TODOS os filmes e séries
+            resultados_finais.extend(list(Serie.objects.all()))
+            resultados_finais.extend(list(Filme.objects.all()))
+
+
+        # 2. LÓGICA DE PESQUISA NORMAL (SE HOUVER UM TERMO)
+        else:
+            # Se houver um termo, faz a busca filtrada
             filtro = Q(titulo__icontains=query)
 
-            # 1. Busca em Séries
             resultados_series = Serie.objects.filter(filtro)
             resultados_finais.extend(list(resultados_series))
 
-            # 2. Busca em Filmes
             resultados_filmes = Filme.objects.filter(filtro)
             resultados_finais.extend(list(resultados_filmes))
 
+        # Retorna a lista combinada (todos, ou filtrados)
         return resultados_finais
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        # O termo de pesquisa pode ser vazio.
         context['termo_pesquisa'] = self.request.GET.get('q', '')
         return context
