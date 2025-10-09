ac9d6b19d6dc8bcba9086596593f3c1de1a6fb88
davi gerson <dgas1@discente.ifpe.edu.br>
2025-10-09 18:20:05 -0300

    adicionado player aos filmes

--- filme/views.py
+++ filme/views.py
@@ -4,19 +4,26 @@ from .models import Filme
 from django.views.generic import TemplateView, ListView, DetailView
 from usuario.models import Usuario
 
-#=========================================================================#
-#class base view
-class Homepage(TemplateView): #vai mostrar a view
+
+# =========================================================================#
+# class base view
+class Homepage(TemplateView):  # vai mostrar a view
     template_name = 'homepage.html'
 
-#filmes
-class Homefilmes(LoginRequiredMixin,ListView):
-        template_name = "homefilmes.html"
-        model = Filme #-> object_list -> lista de itens do modelo
 
-class Detalhesfilme(LoginRequiredMixin,DetailView):
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs) #redireciona para a homepage
+
+
+# filmes
+class Homefilmes(LoginRequiredMixin, ListView):
+    template_name = "homefilmes.html"
+    model = Filme  # -> object_list -> lista de itens do modelo
+
+
+class Detalhesfilme(LoginRequiredMixin, DetailView):
     template_name = "detalhesfilme.html"
-    model = Filme # -> 1 item do modelo
+    model = Filme  # -> 1 item do modelo
 
     # função para contabilizar a visualização
     def get(self, request, *args, **kwargs):
@@ -37,29 +44,29 @@ class Detalhesfilme(LoginRequiredMixin,DetailView):
         context = super(Detalhesfilme, self).get_context_data(**kwargs)  # classe onde ela vai herdar tudo da classe pai
         # filtrar a tabela por series relacionadas por categoria (object)
         # self.get_object() função que vai retornar a variavel object
-        filmes_relacionados = Filme.objects.filter(categoria=self.get_object().categoria)[0:5]  # caso queira controlar os filmes relacionadas, pode usar [0:3]
+        filmes_relacionados = Filme.objects.filter(categoria=self.get_object().categoria)[
+            0:5]  # caso queira controlar os filmes relacionadas, pode usar [0:3]
         context['filmes_relacionados'] = filmes_relacionados
         return context
     # Função para a barra de pesquisa
 
 
-class Pesquisafilme(LoginRequiredMixin,ListView):
+class Pesquisafilme(LoginRequiredMixin, ListView):
     template_name = "pesquisa.html"
     model = Filme  # -> object_list -> lista de itens do modelo
 
-
-#=========================================================================#
+# =========================================================================#
 # Create your views here.
-#função da homepage
-#def homepage(request): #requisição get para a homepage
-#return render(request, "homepage.html")
+# função da homepage
+# def homepage(request): #requisição get para a homepage
+# return render(request, "homepage.html")
 
-#url -view -html
-#cria uma lista de filmes da minha tabela do banco de dados
-#def homefilmes(request):
-#context = {}
-#lista_filmes = Filme.objects.all() #pega todos os filmes do bd
-#context['lista_filmes'] = lista_filmes
-#return render(request, "homefilmes.html", context)
+# url -view -html
+# cria uma lista de filmes da minha tabela do banco de dados
+# def homefilmes(request):
+# context = {}
+# lista_filmes = Filme.objects.all() #pega todos os filmes do bd
+# context['lista_filmes'] = lista_filmes
+# return render(request, "homefilmes.html", context)
 
-#criando uma classe para cada view
+# criando uma classe para cada view
