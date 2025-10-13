c4c38410c0209c471f3c2e7ba45811df53d44ecd
davi gerson <dgas1@discente.ifpe.edu.br>
2025-10-13 17:41:15 -0300

    adicionado recuperar senha e tudo o que podia ser adicionado

--- usuario/urls.py
+++ usuario/urls.py
@@ -2,7 +2,7 @@
 # view - o que vai acontecer quando a pessoa clicar naquele link
 # template - a parte visual que sera exibida
 
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
 from django.contrib.auth import views as auth_view
 
 from usuario.views import Paginaperfil,Criarconta
@@ -18,6 +18,8 @@ urlpatterns = [
     path('logout/', auth_view.LogoutView.as_view(template_name = "logout.html"), name='logout'),  # pk -> primary key
     path('editarperfil/<int:pk>', Paginaperfil.as_view(), name='editarperfil'),  # pk -> primary key
     path('criarconta/', Criarconta.as_view(), name='criarconta'),  # pk -> primary key
+    path('mudarsenha/', auth_view.PasswordChangeView.as_view(template_name="editarperfil.html",
+                                                                    success_url=reverse_lazy('filme:homefilmes')), name='mudarsenha'),  # pk -> primary key
 
     # path('', homepage), #HomePage do site
     # path('filmes/', homefilmes),  # HomePage do site
